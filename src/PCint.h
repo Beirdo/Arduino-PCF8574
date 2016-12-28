376bf054f633a97527e6fac2c808997dce9f4cbd
Gavin Hurlbut <gjhurlbu@gmail.com>
2016-12-27 22:04:16 -0800

    Simplify the PCInt support

--- src/PCint.h
+++ src/PCint.h
@@ -1,7 +1,21 @@
 #ifndef PCINT_H__
 #define PCINT_H__
 
+#include <avr/pgmspace.h>
 #include "pins_arduino.h"
+
+static PROGMEM const uint8_t pcint_to_pin[] = {
+    // PCMSK0 - Port B
+    8, 9, 10, 11, 12, 13, 20, 21,
+    // PCMSK1 - Port C
+    23, 24, 25, 26, 27, 28, 22, 0xFF,
+    // PCMSK2 - Port D
+    0, 1, 2, 3, 4, 5, 6, 7,
+#if 0
+    // PCMSK3 - Port E
+    23, 24, 25, 26
+#endif
+};
 
 volatile uint8_t *port_to_pcmask[] = { &PCMSK0, &PCMSK1, &PCMSK2 };
 
@@ -12,7 +26,8 @@ typedef struct {
     volatile voidFuncPtr userFunc;
 } PCint_t;
 
-uint8_t PCintIndex[24] = {};    // contains index (1-based) into the PCintTable
+// contains index (1-based) into the PCintTable
+uint8_t PCintIndex[NUM_DIGITAL_PINS] = {};
 
 #define MAX_PCINT_COUNT 4
 PCint_t PCintTable[MAX_PCINT_COUNT];  // limit pins supported for memory reasons
@@ -25,24 +40,14 @@ volatile static uint8_t PCintLast[3];
  */
 void PCattachInterrupt(uint8_t pin, void (*userFunc)(void), int mode)
 {
-    uint8_t bit = digitalPinToBitMask(pin);
     uint8_t port = digitalPinToPort(pin);
-    uint8_t slot;
-    volatile uint8_t *pcmask;
-
-    // map pin to PCIR register
     if (port == NOT_A_PORT) {
         return;
-    } else {
-        port -= 2;
-        pcmask = port_to_pcmask[port];
     }
 
-    // deal with analog ports
-    if (port == 1) {
-        slot = port * 8 + (pin - 14);
-    } else {
-        slot = port * 8 + (pin % 8);
+    volatile uint8_t *pcicr = digitalPinToPCICR(pin);
+    if (!pcicr) {
+        return;
     }
 
     if (PCintNext >= MAX_PCINT_COUNT) {
@@ -53,49 +58,50 @@ void PCattachInterrupt(uint8_t pin, void (*userFunc)(void), int mode)
     interrupt->intMode = mode;
     interrupt->userFunc = userFunc;
 
-    PCintIndex[slot] = ++PCintNext;
+    PCintIndex[pin] = ++PCintNext;
  
     // set the mask
-    *pcmask |= bit;
+    volatile uint8_t *pcmask = digitalPinToPCMSK(pin);
+    *pcmask |= digitalPinToPCMSKbit(pin);
+
     // enable the interrupt
-    PCICR |= 0x01 << port;
+    *pcicr |= digitalPinToPCICRbit(pin);
     
-    // Fix init by SkyWodd
-    PCintLast[0] = *portInputRegister(2);
-    PCintLast[1] = *portInputRegister(3);
-    PCintLast[2] = *portInputRegister(4);
+    PCintLast[0] = *portInputRegister(2);   // PORTB
+    PCintLast[1] = *portInputRegister(3);   // PORTC
+    PCintLast[2] = *portInputRegister(4);   // PORTD
 }
 
 void PCdetachInterrupt(uint8_t pin)
 {
-    uint8_t bit = digitalPinToBitMask(pin);
     uint8_t port = digitalPinToPort(pin);
-    volatile uint8_t *pcmask;
-
-    // map pin to PCIR register
     if (port == NOT_A_PORT) {
         return;
-    } else {
-        port -= 2;
-        pcmask = port_to_pcmask[port];
+    }
+
+    volatile uint8_t *pcicr = digitalPinToPCICR(pin);
+    if (!pcicr) {
+        return;
     }
 
     // disable the mask.
-    *pcmask &= ~bit;
+    volatile uint8_t *pcmask = digitalPinToPCMSK(pin);
+    *pcmask &= ~digitalPinToPCMSKbit(pin);
+
     // if that's the last one, disable the interrupt.
     if (*pcmask == 0) {
-        PCICR &= ~(0x01 << port);
+        *pcicr &= ~digitalPinToPCICRbit(pin);
     }
 }
 
 // common code for isr handler. "port" is the PCINT number.
-// there isn't really a good way to back-map ports and masks to pins.
 static void PCint(uint8_t port)
 {
     uint8_t bit;
     uint8_t curr;
     uint8_t mask;
     uint8_t pin;
+    uint8_t pcint;
 
     // get the pin states for the indicated port.
     curr = *portInputRegister(port + 2);
@@ -111,7 +117,11 @@ static void PCint(uint8_t port)
     for (uint8_t i = 0; i < 8; i++) {
         bit = 0x01 << i;
         if (bit & mask) {
-            pin = port * 8 + i;
+            pcint = port * 8 + i;
+            pin = pgm_read_byte(pcint_to_pin + pcint);
+            if (pin == 0xFF) {
+                continue;
+            }
 
             // Trigger interrupt if mode is CHANGE, or if mode is RISING and
             // the bit is currently high, or if mode is FALLING and bit is low.
@@ -132,16 +142,19 @@ static void PCint(uint8_t port)
 
 SIGNAL(PCINT0_vect)
 {
+    // Port B
     PCint(0);
 }
 
 SIGNAL(PCINT1_vect)
 {
+    // Port C
     PCint(1);
 }
 
 SIGNAL(PCINT2_vect)
 {
+    // Port D
     PCint(2);
 }
 
